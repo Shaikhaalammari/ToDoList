fd3036fc2b5e17b5959cecf478cda53f6eb67273
shaikhaalammmari <shaikhaala@live.com>
2020-07-21 23:21:08 +0300

    done we need to fetch

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+let tasks = require("./tasks");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const id = tasks[tasks.length - 1].id + 1;
+  const newTask = { id, ...req.body };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.put("/tasks/:taskId", (req, res) => {
+  const { taskId } = req.params;
+  const foundTask = tasks.find((task) => task.id === +taskId);
+  if (foundTask) {
+    for (const key in req.body) foundTask[key] = req.body[key];
+  } else {
+    res.status(404).json({ message: "you didnt mention this task" });
+  }
+});
+
+app.delete("/tasks/:taskId", async (req, res) => {
+  const { taskId } = req.params;
+  const foundTask = tasks.find((task) => task.id === +taskId);
+  if (foundTask) {
+    tasks = tasks.filter((_task) => _task !== foundTask);
+    res.status(204).end();
+  } else {
+    res.status(404);
+    res.json({ message: "you didnt mention this task" });
+  }
+});
+
+app.listen(8000, () => {
+  console.log(" the project is running on localhost:8000");
+});
---
+++ tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+  {
+    id: 1,
+    task: "wash the car",
+    description: " call car wash home Service",
+    done: false,
+  },
+  {
+    id: 2,
+    task: "clean the room",
+    description: " cause mom wont get angry",
+    done: true,
+  },
+  {
+    id: 3,
+    task: "buy groceries",
+    description: "chocolates, candies, bu6a6",
+    done: true,
+  },
+  {
+    id: 4,
+    task: "start diet",
+    description: " not important",
+    done: false,
+  },
+];
+
+module.exports = tasks;
